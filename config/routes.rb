f284209704f136d1118e5372dd93b24121881b7f
Glen Donnelly <x16127200@student.ncirl.ie>
2017-11-16 22:16:44 +0000

    Hello

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  get 'cart/index'
+
+  resources :items
+  root 'static_pages#home'
+  
+
+  get '/help', to: 'static_pages#help'
+
+  get '/about', to: 'static_pages#about'
+  
+  get '/admin', to: 'user#admin_login'
+  get '/logout', to: 'user#logout'
+  
+  get '/cart', to: 'cart#index'
+  get '/cart/clear', to: 'cart#clearCart'
+  get '/cart/:id', to: 'cart#add'
+  
+  get '/cart/remove/:id' => 'cart#remove'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
